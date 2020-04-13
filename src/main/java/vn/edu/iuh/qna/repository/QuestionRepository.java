66b78f3491b44c4607940b940fc1d9566b2f8342
huuvo <vovanhuu98@gmail.com>
2020-04-13 22:25:19 +0700

    add view my posted,my star, add search function, fixed login function

--- src/main/java/vn/edu/iuh/qna/repository/QuestionRepository.java
+++ src/main/java/vn/edu/iuh/qna/repository/QuestionRepository.java
@@ -1,11 +1,15 @@
 package vn.edu.iuh.qna.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import vn.edu.iuh.qna.entity.CategoryModel;
 import vn.edu.iuh.qna.entity.QuestionModel;
 
 @Repository
 public interface QuestionRepository extends MongoRepository<QuestionModel, String> {
-
+	Page<QuestionModel> findByCategory(CategoryModel category,Pageable pageable);
+	Page<QuestionModel> findByTitleNormalizedContaining(String titleNormalized,Pageable pageable);
 }
